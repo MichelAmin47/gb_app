e9178ed381c2babad89222d6c5787de96c2c1ab1
MichelAmin47 <michelamin@gmail.com>
2023-06-09 23:19:37 +0200

    button interaction

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './air-jordan-logo.png';
-import {quickSearch} from './gb_searcher'
+import {quickSearch, searchGiantBombCastByDate} from './gb_searcher'
 import './App.css';
 import DropdownForm from './components/DropDownForm';
 
@@ -8,17 +8,54 @@ function clickMe() {
   alert(quickSearch());
 }
 
+const options = ["2018-03", "2015-06", "2009-12"];
+
 
 function App() {
+
+  const [count, setCount] = useState(0);
+  const [selected, setSelected] = useState(options[0]);
+  const [countA, setCountA] = useState(0);
+
+  let trialLink:string;
+
+  const submit = () => {
+    console.log(selected);
+    const podcastList = searchGiantBombCastByDate(selected.split("-")[0],selected.split("-")[1])
+    console.log(podcastList);
+    console.log(podcastList[0][2])
+    alert(podcastList[0][2]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <DropdownForm  />  
+        <select 
+          value={selected} 
+          onChange={(e) => setSelected(e.target.value)}>
+          {options.map((value) => (
+          <option value={value} key={value}>
+            {value}
+          </option>
+          ))}
+        </select>
+        <button
+          onClick={() => {
+          submit();
+          setCountA(countA + 1);
+          }}
+        >
+          Submit
+        </button>
+        <p>You selected {selected} </p>
+        <p> {countA}</p>
+        <p> {trialLink} </p>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div>
+          <p>{count}</p>
+          <button onClick={() => setCount(count + 1)}>Increment</button>
+          <button onClick={() => setCount(count - 1)}>Decrement</button>
+        </div>
         <a
           className="App-link"
           href="https://open.spotify.com/episode/3QZvw06TYKqnGL7KDj1FwU"
--- src/gb_searcher.ts
+++ src/gb_searcher.ts
@@ -1,34 +1,48 @@
 import bombCastSpotifyJson from './json/bombcastFromSpotify_COMPLETE.json';
 
 export function searchGiantBombCastByDate(year: string, month: string){
+    let parentPodcastInfo: any[] = [];
+
     for (const key in bombCastSpotifyJson.feed.episodes) {
         if(key.toString().includes(`${year}-${month}`)){
+            let podCastInfo: any[] = [];
             console.log(key);
             // @ts-ignore
             console.log(bombCastSpotifyJson.feed.episodes[key].name);
+            podCastInfo.push(bombCastSpotifyJson.feed.episodes[key].name);
             // @ts-ignore
             console.log(bombCastSpotifyJson.feed.episodes[key].description);
+            podCastInfo.push(bombCastSpotifyJson.feed.episodes[key].description);
             // @ts-ignore
             console.log(bombCastSpotifyJson.feed.episodes[key].external_urls.spotify);
+            podCastInfo.push(bombCastSpotifyJson.feed.episodes[key].external_urls.spotify);
+            parentPodcastInfo.push(podCastInfo);
             console.log("");
         }
     }
+    return parentPodcastInfo;
 }
 
 function searchGiantBombCastDescription(searchParam: string){
+
     for (const key in bombCastSpotifyJson.feed.episodes) {
             // @ts-ignore
             if(bombCastSpotifyJson.feed.episodes[key].description.toString().includes(searchParam)){
+
                 console.log(key);
                 // @ts-ignore
                 console.log(bombCastSpotifyJson.feed.episodes[key].name);
+                
                 // @ts-ignore
                 console.log(bombCastSpotifyJson.feed.episodes[key].description);
+                
                 // @ts-ignore
                 console.log(bombCastSpotifyJson.feed.episodes[key].external_urls.spotify);
+                
                 console.log("");
             }
     }
+
 }
 
 export function quickSearch(){
